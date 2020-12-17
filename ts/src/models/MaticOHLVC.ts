207e2ac741383ceeea6d39e8c15c7dce416a0df3
peter <peter.park13ak@gmail.com>
2020-12-17 14:32:30 -0500

    Add MaticOHLVC model for database

---
+++ ts/src/models/MaticOHLVC.ts
@@ -0,0 +1,22 @@
+export class MaticOHLVC {
+    public dt?: Date;
+    public bid?: number;
+    public ask?: number;
+    public bid_vol?: number;
+    public ask_vol?: number;
+    constructor(
+        opts: {
+            dt?: Date;
+            bid?: number;
+            ask?: number;
+            bid_vol?: number;
+            ask_vol?: number;
+        } = {},
+    ) {
+        this.dt = opts.dt;
+        this.bid = opts.bid;
+        this.ask = opts.ask;
+        this.bid_vol = opts.bid_vol;
+        this.ask_vol = opts.ask_vol;
+    }
+}
